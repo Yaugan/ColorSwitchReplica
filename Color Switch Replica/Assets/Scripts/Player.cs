306a5773ddb75e3b86c422e1c769de8d4d8071e5
Yaugan <yaugansd45@gmail.com>
2020-05-23 08:52:00 +0530

    Color Switch Replica

---
+++ Color Switch Replica/Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float jumpForce = 5f;
+    public Rigidbody2D rb;
+    public string currentColor;
+    public SpriteRenderer sr;
+
+    public Color colorCyan;
+    public Color colorYellow;
+    public Color colorMagenta;
+    public Color colorPink;
+
+    void Start()
+    {
+        SetRandomColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "ColorChanger")
+        {
+            SetRandomColor();
+            Destroy(collision.gameObject);
+            return;
+        }
+
+        if(collision.tag != currentColor)
+        {
+            Debug.Log("GAME OVER !!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void SetRandomColor()
+    {
+        int index = Random.Range(0, 4);
+
+        switch(index)
+        {
+            case 0:
+                currentColor = "Cyan";
+                sr.color = colorCyan;
+                break;
+
+            case 1:
+                currentColor = "Yellow";
+                sr.color = colorYellow;
+                break;
+
+            case 2:
+                currentColor = "Magenta";
+                sr.color = colorMagenta;
+                break;
+
+            case 3:
+                currentColor = "Pink";
+                sr.color = colorPink;
+                break;
+        }
+    }
+}
